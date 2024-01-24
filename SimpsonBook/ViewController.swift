c2ecf0c8f6fd94d944c29570c09ad18e7f0b4fb5
Fırat Akbulut <frtakbulut@gmail.com>
2024-01-24 16:44:59 +0300

    first commit

---
+++ SimpsonBook/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  SimpsonBook
+//
+//  Created by Atil Samancioglu on 11.07.2019.
+//  Copyright © 2019 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var mySimpson = [Simpson]()
+    var chosenSimpson: Simpson?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        let homer = Simpson(name: "Homer Simpson", job: "Nuclear Safety", image: UIImage(named: "homer")!)
+        let marge = Simpson(name: "Marge Simpson", job: "Housew ife", image: UIImage(named: "marge")!)
+        let bart = Simpson(name: "Bart Simpson", job: "Student", image: UIImage(named: "bart")!)
+        let lisa = Simpson(name: "Lisa Simpson", job: "Student", image: UIImage(named: "lisa")!)
+        let maggie = Simpson(name: "Maggie Simpson", job: "Baby", image: UIImage(named: "maggie")!)
+        
+        mySimpson.append(homer)
+        mySimpson.append(marge)
+        mySimpson.append(bart)
+        mySimpson.append(lisa)
+        mySimpson.append(maggie)
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mySimpson.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = mySimpson[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenSimpson = mySimpson[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC"{
+            let destinationVc = segue.destination as! detailsVC
+            destinationVc.selectedSimpson = chosenSimpson
+        }
+    }
+    
+}
+
